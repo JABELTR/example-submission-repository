5efe0dcbf81231d040df3d4d08b597219c829584
JABS <jairo.beltrans@gmail.com>
2023-12-21 19:49:53 -0500

    Sexto Commit

--- part1/increment/src/App.jsx
+++ part1/increment/src/App.jsx
@@ -4,46 +4,58 @@ const History = (props) => {
   if (props.allClicks.length === 0) {
     return (
       <div>
-        the app is used by pressing the buttons
+        *** This App is used by pressing the buttons ***
       </div>
     )
   }
 
   return (
     <div>
-      button press history: {props.allClicks.join(' ')}
+      <h1>Total clicks {props.allClicks.length} </h1> 
     </div>
   )
 }
 
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
   </button>
 )
 
 const App = () => {
-  const [left, setLeft] = useState(0)
-  const [right, setRight] = useState(0)
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
   const [allClicks, setAll] = useState([])
 
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'))
-    setLeft(left + 1)
+  const handleGoodClick = () => {
+    setAll(allClicks.concat('G'))
+    setGood(good + 1)
   }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'))
-    setRight(right + 1)
+  const handleNeutralClick = () => {
+    setAll(allClicks.concat('N'))
+    setNeutral(neutral + 1)
+  }
+
+  const handleBadClick = () => {
+    setAll(allClicks.concat('B'))
+    setBad(bad + 1)
   }
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text='left' />
-      <Button handleClick={handleRightClick} text='right' />
-      {right}
-      <History allClicks={allClicks} />
+      <h1>Give feedback</h1>
+      <Button handleClick={handleGoodClick} text='Good' />
+      <Button handleClick={handleNeutralClick} text='Neutral' />
+      <Button handleClick={handleBadClick} text='Bad' /><br></br>
+      <br></br>
+      <br></br>
+      <h1>Statistics</h1><br></br>
+      <h2>Good {good}</h2><br></br>
+      <h2>Neutral {neutral}</h2><br></br>
+      <h2>Bad {bad}</h2><br></br>
+      <History allClicks={allClicks}/>
     </div>
   )
 }
